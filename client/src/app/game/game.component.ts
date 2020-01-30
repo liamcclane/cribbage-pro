efe86336a7c75c5d06cea21f5467bb99af86a156
liamcclane <lia_mcclane@msn.com>
2020-01-29 23:08:00 -0800

    board design done

--- client/src/app/game/game.component.ts
+++ client/src/app/game/game.component.ts
@@ -29,7 +29,7 @@ export class GameComponent implements OnInit {
 
   ngOnInit() {
     this.startGame();
-    for (let i = -1; i < 120; i++) {
+    for (let i = -1; i < 121; i++) {
       if(i>80) this.scoreDivs80to120.unshift(i);
       else if(i>40)this.scoreDivs40to80.push(i);
       else this.scoreDivs1tp40.unshift(i);
