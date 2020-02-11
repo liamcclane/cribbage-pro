3f7a1edf9bb7cd58671392ea677a48425dd57de0
Brendan Stanton <bmsenate@gmail.com>
2020-02-11 13:06:16 -0800

    fixed up routing, started tutorial

--- client/src/app/game/game.component.ts
+++ client/src/app/game/game.component.ts
@@ -11,11 +11,44 @@ import { Judge } from '../judge';
 })
 export class GameComponent implements OnInit {
 
+  scorePegHole80to120: number[] = [];
+  scorePegHole40to80: number[] = [];
+  scorePegHole1to40: number[] = [];
+  scorePegHole1to30: number[] = [];
+  scorePegHole30to60: number[] = [];
+  scorePegHole60to90: number[] = [];
+  scorePegHole90to120: number[] = [];
+  scorePegHoles: number[] = [];
+  // messageOfTheDay = this.messages[Math.floor(Math.random() * this.messages.length)];
+
   constructor() { }
 
   ngOnInit() {
+    this.getPegs();
 
+  }
 
+  getPegs() {
+    for (let i = -2; i <= 121; i++) {
+      this.scorePegHoles.push(i);
+      if (i > 80) {
+        this.scorePegHole80to120.push(i);
+      } else if (i > 40) {
+        this.scorePegHole40to80.push(i);
+      } else {
+        this.scorePegHole1to40.push(i);
+      }
+
+      if (i > 90) {
+        this.scorePegHole90to120.push(i);
+      } else if (i > 60) {
+        this.scorePegHole60to90.push(i);
+      } else if (i > 30) {
+        this.scorePegHole30to60.push(i);
+      } else {
+        this.scorePegHole1to30.push(i);
+      }
+    }
   }
 
 }
